98c4d7e1d1aa103a6a3c76fc8e3c99ffbd44631f
aabbas99 <aabbas99@terpmail.umd.edu>
2023-08-03 09:52:58 -0700

    5

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):].split("with ")[1])
+str = str[39:67] + str[115:120] + str[:6]
+print(str)
