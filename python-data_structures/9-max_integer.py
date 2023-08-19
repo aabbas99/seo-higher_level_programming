ca00baa059c125a7d3497172ce1ae055331391fc
aabbas99 <aabbas99@terpmail.umd.edu>
2023-08-18 20:57:17 -0700

    2

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return "None"
     max = my_list[0]
     for item in my_list:
-        if my_list[i] > max:
-            max = my_list[i]
+        if item > max:
+            max = item
     return max
