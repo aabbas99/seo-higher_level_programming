42834d9278ca06c91cb7f6113b28acb946e8e552
aabbas99 <aabbas99@terpmail.umd.edu>
2023-08-18 21:15:44 -0700

    3

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for i in my_list:
+    result = my_list[:]
+    for i in enumerate(my_list):
         if i % 2 != 0:
-            result.append("False")
+            result[i] = False
         else:
-            result.append("True")
+            result[i] = True
     return result
