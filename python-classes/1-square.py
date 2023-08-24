0c45f5f0318705697abc662fc642930c3fa00aee
aabbas99 <aabbas99@terpmail.umd.edu>
2023-08-23 20:47:04 -0700

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+        """Represent a Square."""
+        
+
+        def __init__(self, size):
+            """Initalize a new Square.
+
+            Args:
+                size(int) The size of the new square.
+            """
+            self.__size = size
