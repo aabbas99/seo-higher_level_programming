d76ffbcfd658e2ba2407909a4a4339d67d9a9794
aabbas99 <aabbas99@terpmail.umd.edu>
2023-08-30 13:43:39 -0700

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+Define a class Square.
+"""
 
 
 class Square:
-    """Represent a Square."""
+    """
+    Represent a Square.
+    """
 
     def __init__(self, size = 0):
         """
